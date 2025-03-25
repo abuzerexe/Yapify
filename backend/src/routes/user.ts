233dbcb3e0e50568c290f86517affa0a10d5a16c
abuzerexe <abouzer381@gmail.com>
2025-03-25 10:44:18 +0500

    Signin route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import {Hono} from 'hono';
 import { Jwt } from 'hono/utils/jwt';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import {createHash, sha256} from 'hono/utils/crypto';
+import { sha256} from 'hono/utils/crypto';
 
 
 const user = new Hono<{
@@ -53,10 +53,9 @@ user.post('/signup', async (c)=>{
             }
         })
 
-        const token = await Jwt.sign(result,c.env.JWT_SECRET)
 
         return c.json({
-            token
+            message : "Sign up Successfully.",
         })
 
     }catch(e:any){
@@ -70,6 +69,48 @@ user.post('/signup', async (c)=>{
 
 })
 
-user.post('/signin', )
+user.post('/signin', async (c)=>{
+
+    const body = await c.req.json();
+    const email = body['email'];
+    const password = body['password'];
+
+    const hashedPassword = await sha256(password)
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+    
+    try{
+
+    
+        const check = await prisma.user.findFirst({
+            where :{
+                email,
+                password : hashedPassword as string
+            },
+            select: {
+                id : true
+            }
+        })
+
+        if(check){
+
+            const token = await Jwt.sign(check,c.env.JWT_SECRET)
+
+            return c.json({
+                message : "Sign In Successfully.",
+                token
+            })
+        }
+    }catch(e:any){
+        c.status(411)
+       return c.json({
+            message : "Error while signing in. Try again.",
+            error : e.message
+        })
+    }
+
+} )
 
 export default user;
