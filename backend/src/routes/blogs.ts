bd57b4024e6dcfe2960c5745194309cfec852557
abuzerexe <abouzer381@gmail.com>
2025-03-27 08:13:35 +0500

    middleware

--- backend/src/routes/blogs.ts
+++ backend/src/routes/blogs.ts
@@ -1,16 +1,69 @@
 import {Hono} from 'hono';
+import { Jwt } from 'hono/utils/jwt';
 
-const blog = new Hono();
+const blog = new Hono<{
+    Bindings:{
+        DATABASE_URL : string,
+        JWT_SECRET : string
+       },
+    Variables : {
+        userId : string
+    }
+}>();
 
-blog.post('/')
+blog.use('/*', async (c,next)=>{
+
+    try{
+        const auth = c.req.header('Authorization')
+
+        if(!auth){
+            c.status(403)
+            return c.json({
+                error : "Unauthorized"
+            })
+        }
+
+        const token = auth?.split(' ')[1]
+
+        const payload = await Jwt.verify(token as string,c.env.JWT_SECRET)
+
+        if(!payload){
+            c.status(403)
+            return c.json({
+                error : "Unauthorized"
+            })
+        }
+
+        c.set('userId', payload.id as string)
+
+        await next();
+
+    }catch(e:any){
+        c.status(403);
+       return c.json({
+            message : "You are not Authorized.",
+            error : e.message
+        })
+    }
+})
+
+
+blog.post('/',(c)=>{
+    const id = c.get('userId')
+    return c.json({
+        msg : 'test'
+    })
+})
 
 blog.put('/')
+
 
 blog.get('/:id',(c)=>{
     const id = c.req.param('wd');
     console.log(id)
     return c.text(`id : ${id}`)
 })
+
 
 blog.get('/bulk')
 
