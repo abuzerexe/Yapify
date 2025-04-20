3ecdcf988fba72377680e7a2f498afe1920f26e8
abuzerexe <abouzer381@gmail.com>
2025-04-20 16:56:24 +0500

    hooks

---
+++ frontend/src/hooks/useBlog.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
+export const useBlog = ({id}:{id:string}) =>{
+    const {isPending, isError, data, error} = useQuery({
+        queryKey: ['blog'],
+        queryFn : async ()=>{
+            const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/blogs/${id}`)
+            return response.data;
+        }
+    })
+
+    return {
+        isPending,
+        isError,
+        blog : data,
+        error
+    }
+}
--- frontend/src/hooks/useBlogs.tsx
+++ frontend/src/hooks/useBlogs.tsx
@@ -1,8 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 export const useBlogs = ()=>{
     const { isPending, isError, data, error } = useQuery({
-        queryKey = ['blogs'],
-        queryFn = axios.g
+        queryKey : ['blogs'],
+        queryFn : async ()=>{
+            const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/blogs`)
+            return response.data.data
+        }
     })
+    return{
+        isPending,
+        isError,
+        blogs : data,
+        error
+    }
 }
