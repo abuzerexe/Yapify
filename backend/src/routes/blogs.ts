59ddf16886b8c3473be0f6b557b1c21df0f0666a
abuzerexe <abouzer381@gmail.com>
2025-04-28 01:15:45 +0500

    change in createdat type

--- backend/src/routes/blogs.ts
+++ backend/src/routes/blogs.ts
@@ -1,3 +1,4 @@
+import { formatISO } from 'date-fns'; 
 import {  PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import {Hono} from 'hono';
@@ -8,59 +9,57 @@ const blogs = new Hono<{
        }
 }>();
 
-blogs.get('/:id',async (c)=>{
-
+blogs.get('/:id', async (c) => {
     const blogId = c.req.param('id');
 
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())
+    }).$extends(withAccelerate());
 
-    try{
+    try {
         const response = await prisma.blog.findFirst({
-            where : {
-                id: blogId
+            where: {
+                id: blogId,
             },
-            select:{
+            select: {
                 title: true,
                 content: true,
-                createdAt:true,
-                id : true,
-                author:{
+                createdAt: true,
+                id: true,
+                author: {
                     select: {
                         id: true,
                         name: true,
-                        email: true
-                    }
-                }
-            }
-        })
-    
-        if(response){
+                        email: true,
+                    },
+                },
+            },
+        });
+
+        if (response) {
             return c.json({
-                data : response
-            })
-    
-        }else{
-            c.status(411)
+                data: {
+                    ...response,
+                    createdAt: formatISO(new Date(response.createdAt)), // Convert to ISO string
+                },
+            });
+        } else {
+            c.status(411);
             return c.json({
-                message : "Error while getting blog.",
-            })
+                message: "Error while getting blog.",
+            });
         }
-    
-    }catch(e:any){
-
-        c.status(411)
+    } catch (e: any) {
+        c.status(411);
         return c.json({
-            message : "Error while getting blog.",
-            error : e.message
-        })
+            message: "Error while getting blog.",
+            error: e.message,
+        });
     }
-
 })
 
 blogs.get('/', async(c)=>{
-    
+
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
