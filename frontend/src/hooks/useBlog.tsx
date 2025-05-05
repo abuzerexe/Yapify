53ccf8df444a9e8936e3007bab02021a045be987
abuzerexe <abouzer381@gmail.com>
2025-05-05 23:07:03 +0500

    improved caching

--- frontend/src/hooks/useBlog.tsx
+++ frontend/src/hooks/useBlog.tsx
@@ -3,7 +3,7 @@ import axios from "axios"
 
 export const useBlog = ({id}:{id:string}) =>{
     const {isPending, isError, data, error} = useQuery({
-        queryKey: ['blog'],
+        queryKey: ['blog',id],
         queryFn : async ()=>{
             const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/blogs/${id}`)
             return response.data;
--- frontend/src/hooks/useBlogs.tsx
+++ frontend/src/hooks/useBlogs.tsx
@@ -7,7 +7,8 @@ export const useBlogs = ()=>{
         queryFn : async ()=>{
             const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/blogs`)
             return response.data.data
-        }
+        },
+        staleTime : 60 * 1000
     })
     return{
         isPending,
